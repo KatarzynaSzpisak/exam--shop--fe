b3a551c3fe0cc8ea3cd16c397fa97cd7b4b3b38c
Lela <44258601+KatarzynaSzpisak@users.noreply.github.com>
2020-07-24 14:18:10 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,85 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'c/d Urinary Care Multicare med kyckling',
+      category: 'Vetfood',
+      image: '/images/hills urinary.jpg',
+      price: 179,
+      brand: ' Hills Prescription Diet Feline',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 's/d Urinary Care med kyckling',
+      category: 'Vetfood',
+      image: '/images/s d hills.jpg',
+      price: 209,
+      brand: ' Hills Prescription Diet Feline',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Metabolic + Urinary Stress',
+      category: 'Vetfood',
+      image: '/images/metab+urinar hills.jpg',
+      price: 249,
+      brand: ' Hills Prescription Diet Feline',
+      rating: 4.5,
+      numReviews: 8
+    }, {
+      _id: '4',
+      name: 'k/d Kidney Care',
+      category: 'Vetfood',
+      image: '/images/k d hills.jpg',
+      price: 209,
+      brand: ' Hills Prescription Diet Feline',
+      rating: 4.5,
+      numReviews: 8
+    },
+    {
+      _id: '5',
+      name: 'c/d Multicare Urinary Care',
+      category: 'Vetbfood',
+      image: '/images/s d hills.jpg',
+      price: 20,
+      brand: ' Hills Prescription Diet Feline',
+      rating: 4.5,
+      numReviews: 8
+    },
+    {
+      _id: '6',
+      name: 'c/d Urinary Stress Stew med kyckling',
+      category: 'Vetbfood',
+      image: '/images/cd bl.jpg',
+      price: 15,
+      brand: ' Hills Prescription Diet Feline',
+      rating: 4.5,
+      numReviews: 8
+    },
+    {
+      _id: '7',
+      name: 'i/d Digestive Care Chicken',
+      category: 'Vetbfood',
+      image: '/images/id bl.jpg',
+      price: 25,
+      brand: ' Hills Prescription Diet Feline',
+      rating: 4.5,
+      numReviews: 8
+    },
+    {
+      _id: '8',
+      name: 'z/d Food Sensitivities Original',
+      category: 'Vetbfood',
+      image: '/images/k d hills.jpg',
+      price: 25,
+      brand: ' Hills Prescription Diet Feline',
+      rating: 4.5,
+      numReviews: 8
+    },
+    
+    
+  ]
+}
